fa543bce0edd7359de588be252f28a8f83d22465
Charles Fulton <fultonc@lafayette.edu>
2019-05-18 13:04:17 -0400

    Add PHPDocs information

--- index.php
+++ index.php
@@ -15,6 +15,8 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
+ * Main administration page for the tool. 
+ *
  * @package   tool_rssfeeds
  * @copyright 2018 Lafayette College ITS
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
