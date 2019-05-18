1a2e55a501d2a28ebc3043403c4303a1cb5dd735
Charles Fulton <fultonc@lafayette.edu>
2019-05-18 13:04:41 -0400

    Manage all RSS feeds v3.4.1

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018112800;
+$plugin->version   = 2018112801;
 $plugin->requires  = 2017111300;
 $plugin->component = 'tool_rssfeeds';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = 'v3.4.0';
+$plugin->release   = 'v3.4.1';
